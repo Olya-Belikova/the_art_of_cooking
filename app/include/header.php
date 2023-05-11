9fab966af0d70db5e06e5f86927c92d4d7e8edbf
Натальина Дарья Александровна <natalina.daria@gmail.com>
2023-05-11 19:00:17 +0300

    v3
    Настроен гитхаб

--- app/include/header.php
+++ app/include/header.php
@@ -1,42 +1,43 @@
 <header class="container-fluid">
     <div class="container">
         <div class="row">
-            <div class="col-4">
-                <h1>
-                    <a href="<?php echo BASE_URL ?>">My blog</a>
-                </h1>
+            <div id="logo" class="col-4">
+                    <a href="<?php echo BASE_URL ?>">
+                        <img src="assets/logo.svg" alt="logo" height="55px" />
+                    </a>
             </div>
-            <nav class="col-8">
+            
+            <nav class="col-7">
                 <ul>
                     <li><a href="<?php echo BASE_URL ?>">Главная</a> </li>
-                    <li><a href="<?php echo BASE_URL . 'about.php'?>">О нас</a> </li>
-                    <li><a href="<?php echo BASE_URL . 'catalog.php'?>">Каталог</a> </li>
-
-                    <li>
-                        <?php if (isset($_SESSION['id'])): //если в $_SESSION есть параметр id, то...?>
+                    <li><a href="<?php echo BASE_URL . 'about.php'?>">О блоге</a> </li>
+                    <li><a href="<?php echo BASE_URL . 'catalog.php'?>">Рецепты</a> </li>
+                </ul>
+            </nav>
+            <ul class="col-1">
+                <li id="login">
+                        <?php if (isset($_SESSION['id'])): ?>
                             <a href="#">
-                                <i class="fa fa-user"></i>
-                                <?php echo $_SESSION['login']; //печатаеться логин пользователя?>
+                                
+                                <?php echo $_SESSION['login']; ?>
                             </a>
                             <ul>
-                                <?php if ($_SESSION['admin']): //если в $_SESSION парамете admin, то...?>
-                                    <li><a href="#">Админ панель</a> </li> <!--эта панель есть-->
+                                <?php if ($_SESSION['admin']): ?>
+                                    <li><a href="#">Админ панель</a> </li>
                                 <?php endif; ?>
                                 <li><a href="<?php echo BASE_URL . "logout.php"; ?>">Выход</a> </li>
                             </ul>
                         <?php else: ?>
                             <a href="<?php echo BASE_URL . "log.php"; ?>">
-                                <i class="fa fa-user"></i>
+                                
                                 Войти
                             </a>
                             <ul>
                                 <li><a href="<?php echo BASE_URL . "reg.php"; ?>">Регистрация</a> </li>
                             </ul>
-                        <?php endif; //конец?>
-
-                    </li>
-                </ul>
-            </nav>
+                        <?php endif; ?>
+                </li>
+            </ul>
         </div>
     </div>
 </header>
