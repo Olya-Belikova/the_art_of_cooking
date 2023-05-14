70368bb12e7afb12d322a81fc7b5a6c857978df4
Olya-Belikova <olyabelickova@gmail.com>
2023-05-14 18:55:29 +0300

    V6

--- path.php
+++ path.php
@@ -1,4 +1,4 @@
-<?php //'функция', отвечающая за корневую дирректорию, за расположение блока(сервера)
+<?php //'функция', отвечающая за корневую директорию, за расположение блока(сервера)
 error_reporting(E_ERROR | E_PARSE);
 const SITE_ROOT = __DIR__;
 const BASE_URL = 'http://localhost/the_art_of_cooking/';
